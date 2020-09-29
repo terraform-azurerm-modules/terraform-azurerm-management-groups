804a73c25c9e1d1625e795fb59b833fd87794340
Matt White <matt.white@microsoft.com>
2020-09-29 20:59:26 +0100

    move comment

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ data "azurerm_subscription" "current" {}
 locals {
   parent               = var.parent_management_group_name != null ? true : false
   tenant_root_group_id = "/providers/Microsoft.Management/managementGroups/${data.azurerm_subscription.current.tenant_id}"
-  // This local checks for duplicate subscription IDs in the CSV
-  // It is not used in the plan but is useful to prevent failures on apply
   subscription_to_mg_csv_data_check = {
+    // This local checks for duplicate subscription IDs in the CSV
+    // It is not used in the plan but is useful to prevent failures on apply
     for s in var.subscription_to_mg_csv_data :
     s.subId => s.mgName
   }
